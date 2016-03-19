85007cfea1c7a9476973c9e8531e2391a6a6fdab
Joffrey Bienvenu <joffreybvn@gmail.com>
2016-03-19 23:08:03 +0100

    Rewrite of the display's function. Format now the sudoku to be easily readable.

--- display.py
+++ display.py
@@ -2,9 +2,17 @@
 
 
 def print_sudoku(data_map):
+    print '-------------------'
+
     for i in range(1, 10):
-        line = ''
+        line = '| '
+
         for j in range(1, 10):
             line += str(data_map.get((i, j), 0))
+            if not j % 3:
+                line += ' | '
+
+        if not i % 3:
+            line += '\n-------------------'
+
         print line
-    print '\n'
